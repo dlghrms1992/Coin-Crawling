d54b91b8efa65adb0466377eb49d792f8836ba8d
dcthg <dct_hg09@coolschool.co.kr>
2023-09-20 09:51:18 +0900

    크롤링 api 연동정

--- src/Components/Common/Header.tsx
+++ src/Components/Common/Header.tsx
@@ -1,46 +1,61 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Button, ButtonGroup, Form, Navbar, ToggleButton} from 'react-bootstrap';
+import {Button, ButtonGroup, Form, ToggleButton} from 'react-bootstrap';
 import { selectExchangeArr } from '../../constants';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Navbar from 'react-bootstrap/Navbar';
+
 // import {useState} from "react";
 // const [radioValue, setRadioValue] = useState('1');
 
 const Header: React.FC = () => {
     return (
         <header>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <ButtonGroup>
-                                {selectExchangeArr.map((exchange, idx) => (
-                                    <ToggleButton
-                                        key={idx}
-                                        id={`exchange-${idx}`}
-                                        type="radio"
-                                        variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-                                        name="radio"
-                                        value={exchange.value}
-                                        // checked={radioValue === exchange.value}
-                                        // onChange={(e) => setRadioValue(e.currentTarget.value)}
-                                    >
-                                        {exchange.name}
-                                    </ToggleButton>
-                                ))}
-                            </ButtonGroup>
-                        </Col>
-                        <Col xs={6} md={5}>
-                            <Form.Control size="lg" type="text" placeholder="검색어를 입력해주세요!" />
-                        </Col>
-                        <Col xs="auto" md={1}>
-                            <Button variant="primary">검색</Button>
-                        </Col>
-                        <Col xs={3} md={2}>
-                            <Button variant="primary">사용자 정보나 알림?</Button>
-                        </Col>
-                    </Row>
-                </Container>
+            <Navbar className="bg-body-tertiary " bg="dark" data-bs-theme="dark">
+                <Navbar.Brand href="#">
+                    <ButtonGroup>
+                        {selectExchangeArr.map((exchange, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                id={`exchange-${idx}`}
+                                type="radio"
+                                variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                                name="radio"
+                                value={exchange.value}
+                                // checked={radioValue === exchange.value}
+                                // onChange={(e) => setRadioValue(e.currentTarget.value)}
+                            >
+                                {exchange.name}
+                            </ToggleButton>
+                        ))}
+                    </ButtonGroup>
+                </Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+
+                    <Form >
+                        <Row>
+                            <Col xs="auto">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Search"
+                                    className=" mr-sm-2"
+                                />
+                            </Col>
+                            <Col xs="auto">
+                                <Button type="submit">Submit</Button>
+                            </Col>
+                            <Col xs="auto">
+                                <Button variant="outline-success">로그인</Button>
+                            </Col>
+                            <Col xs="auto">
+                                <Button variant="outline-success">알림 </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Navbar.Collapse>
             </Navbar>
         </header>
     );
